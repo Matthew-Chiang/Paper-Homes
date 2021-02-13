379f9e98ec331107767df309de78ec966afdd7b0
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2021-02-13 00:21:11 -0800

    initial dashboard

--- paper-homes/src/App.js
+++ paper-homes/src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './pages/SignUp'
+//import DashboardLayout from './layouts/DashboardLayout'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Main from './components/Main'
 
-function App() {
+class App extends Component {
+  render(){
   return (
+    <div className="wrapper">
+      <Router>
+        <Sidebar />
+        <Route path='/' component={Main} />
+      </Router>
+    </div>
+    /*
     <div className="App">
       <SignUp/>
     </div>
+    */
   );
+  }
 }
 
 export default App;
