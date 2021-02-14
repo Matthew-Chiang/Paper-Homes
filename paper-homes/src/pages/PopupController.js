3224b4c90c74c320cb56b7d08731c2848b3978a2
Matthew <matthew.chiang.myc@gmail.com>
2021-02-14 03:21:30 -0500

    fixed dialog flashing

--- paper-homes/src/pages/PopupController.js
+++ paper-homes/src/pages/PopupController.js
@@ -5,15 +5,57 @@ import Popup1 from "../components/Popup1";
 import Popup2 from "../components/Popup2";
 import Popup3 from "../components/Popup3";
 import { UserContext } from "../context/userContext";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function SignUp() {
-    // const classes = useStyles();
+    const useStyles = makeStyles((theme) => ({
+        form: {
+            display: "flex",
+            flexDirection: "column",
+            margin: "auto",
+            width: "fit-content",
+        },
+        formControl: {
+            marginTop: theme.spacing(2),
+            minWidth: 120,
+        },
+        formControlLabel: {
+            marginTop: theme.spacing(1),
+        },
+        main: {
+            padding: "0px 60px",
+            textAlign: "center",
+            marginLeft: "auto",
+            marginRight: "auto",
+        },
+        formFlex: {
+            display: "flex",
+            alignItems: "center",
+            alignContent: "center",
+            padding: "20px",
+            marginLeft: "auto",
+            marginRight: "auto",
+        },
+    }));
     const [page, setPage] = useState(1);
     const [data, setData] = useState({});
 
-    const renderSwitch = (param, setPage) => {
+    const [open, setOpen] = React.useState(true);
 
-    // }, user, setUser) => {
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const renderSwitch = (param, setPage) => {
+        // }, user, setUser) => {
         switch (param) {
             case 1:
                 return (
@@ -53,10 +95,21 @@ export default function SignUp() {
         //         <div>{renderSwitch(page, setPage, user, setUser)}</div>
         //     )}
         // </UserContext.Consumer>
-        <div>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="max-width-dialog-title"
+            fullWidth={true}
+            maxWidth="md"
+        >
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Close
+                </Button>
+            </DialogActions>
             {renderSwitch(page, setPage)}
-        </div>
-        
-            // , user, setUser)}
+        </Dialog>
+
+        // , user, setUser)}
     );
 }
