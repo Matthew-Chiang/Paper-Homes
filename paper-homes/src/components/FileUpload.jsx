e66a8eeb2282e3421160f90790d331eab0c65bbf
Matthew <matthew.chiang.myc@gmail.com>
2021-02-14 07:50:54 -0500

    uploading csv of addresses

--- paper-homes/src/components/FileUpload.jsx
+++ paper-homes/src/components/FileUpload.jsx
@@ -2,53 +2,63 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 import axios from "axios";
-import 'date-fns';
+import "date-fns";
 // import * as React from 'react'
 // import CSVReader from 'react-csv-reader'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      
-    },
+    root: {},
     uploadButton: {
-        backgroundColor: '#5C7294',
-        color: '#fff',
-        borderRadius: '25px',
-        marginTop: '20px',
-        boxShadow: 'none',
-        '&:hover': {
-            backgroundColor: '#1A2F4F',
-            boxShadow: 'none',
-          },
-        '&:active': {
-            backgroundColor: '#1A2F4F',
-            boxShadow: 'none',
+        backgroundColor: "#5C7294",
+        color: "#fff",
+        borderRadius: "25px",
+        marginTop: "20px",
+        boxShadow: "none",
+        "&:hover": {
+            backgroundColor: "#1A2F4F",
+            boxShadow: "none",
+        },
+        "&:active": {
+            backgroundColor: "#1A2F4F",
+            boxShadow: "none",
         },
     },
-  }));
-  
-  export default function FileUploader() {
+}));
+
+export default function FileUploader({ setCsvInfo }) {
     const classes = useStyles();
-  
+    let fileReader;
+
+    const handleFileRead = (e) => {
+        console.log("asdf");
+        const content = fileReader.result;
+        setCsvInfo(content);
+    };
+
+    const handleFileInput = (e) => {
+        const uploadedFile = e.target.files[0];
+        console.log(uploadedFile);
+        if (uploadedFile.type == "text/csv") {
+            fileReader = new FileReader();
+            fileReader.onloadend = handleFileRead;
+            fileReader.readAsText(uploadedFile);
+        }
+    };
+
     return (
-      <div className={classes.root}>
-        <Button className={classes.uploadButton}
-        variant="contained"
-        component="label"
-        >       
-         + Upload
-         <input
-            type="file"
-            hidden
-        />
-        </Button>
-        {/* <CSVReader
+        <div className={classes.root}>
+            <Button
+                className={classes.uploadButton}
+                variant="contained"
+                component="label"
+            >
+                + Upload
+                <input type="file" onChange={handleFileInput} hidden />
+            </Button>
+            {/* <CSVReader
   parserOptions={{ header: true }}
   onFileLoaded={(data, fileInfo) => console.dir(data, fileInfo)}
 /> */}
-      </div>
+        </div>
     );
-  }
-
-  
-  
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
 var firebase = require("firebase/app");
-require("firebase/firestore");  
+require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtY3EfI2UIPsiSG-9PJrEQWclJ1ppAmdg",
-  authDomain: "paperhomes-44719.firebaseapp.com",
-  projectId: "paperhomes-44719",
-  storageBucket: "paperhomes-44719.appspot.com",
-  messagingSenderId: "1005204187826",
-  appId: "1:1005204187826:web:49b167432e401c891977ab"
+    apiKey: "AIzaSyDtY3EfI2UIPsiSG-9PJrEQWclJ1ppAmdg",
+    authDomain: "paperhomes-44719.firebaseapp.com",
+    projectId: "paperhomes-44719",
+    storageBucket: "paperhomes-44719.appspot.com",
+    messagingSenderId: "1005204187826",
+    appId: "1:1005204187826:web:49b167432e401c891977ab",
 };
 firebase.initializeApp(firebaseConfig);
 
@@ -44,77 +44,53 @@ const db = firebase.firestore();
 // }
 
 async function addUserData(db, data) {
-  const docRef = db.collection('users').doc(data['email']);
-  await docRef.set(data);
-  await docRef.update({createddate:"2021-02-14"});
+    const docRef = db.collection("users").doc(data["email"]);
+    await docRef.set(data);
+    await docRef.update({ createddate: "2021-02-14" });
 }
 
-async function quickstartListen(db,data) {
-  // [START quickstart_listen]
-  // [START firestore_setup_dataset_read]
-  //const snapshot = await db.collection('users').doc(data).get();
-  const userRef = db.collection('users').doc(data);
-  // console.log(userRef);
-  const doc = await userRef.get()
-  const user = await doc.data();
-  // if (!doc.exists) {
-  //   console.log('No such document!');
-  // } else {
-  //   console.log('Document data:', doc.data());
-  // }
+async function quickstartListen(db, data) {
+    // [START quickstart_listen]
+    // [START firestore_setup_dataset_read]
+    //const snapshot = await db.collection('users').doc(data).get();
+    const userRef = db.collection("users").doc(data);
+    // console.log(userRef);
+    const doc = await userRef.get();
+    const user = await doc.data();
+    // if (!doc.exists) {
+    //   console.log('No such document!');
+    // } else {
+    //   console.log('Document data:', doc.data());
+    // }
 
-  return doc.data();
-  
-  // [END firestore_setup_dataset_read]
-  // [END quickstart_listen]
+    return doc.data();
+
+    // [END firestore_setup_dataset_read]
+    // [END quickstart_listen]
 }
-
 
 /* GET home page. */
-router.post('/user', function(req, res, next) {
-  data = req.body
-  console.log(data);
+router.post("/user", function (req, res, next) {
+    data = req.body;
+    console.log(data);
 
-  addUserData(db, data)
-  res.send("user");
-
-  
-  
+    addUserData(db, data);
+    res.send("user");
 });
 router.get("/user/:user_id", function (req, res, next) {
-  const user_id = req.params.user_id;
-  data = req.body;
-  // console.log(user_id);
-
-  // user = quickstartListen(db, user_id);
-  // (async()=> {
-  //   console.log(await quickstartListen(db,user_id))
-  // })()
-  quickstartListen(db,user_id).then(user =>{
-    res.send(user);
-  }) 
-  //res.send("user");//quickstartListen(db, user_id));
+    const user_id = req.params.user_id;
+    data = req.body;
+    quickstartListen(db, user_id).then((user) => {
+        res.send(user);
+    });
 });
-/* GET user page. */
-/*
-router.getuser('/user', function(req, res, next) {
-  data = req.body
-  console.log(data);
 
-  quickstartListen(db)
-  res.send("user");
-
+router.post("/address/createAddresses", async function (req, res, next) {
+    const addresses = req.body.addresses;
+    addresses.forEach(async (address) => {
+        const docRef = db.collection("address").doc(address.address);
+        await docRef.set(address);
+    });
 });
-*/
-
-// /* GET home page. */
-// router.put('/user', function(req, res, next) {
-//   data = req.body
-//   console.log(data);
-
-//   addUserData(db, data)
-//   res.send("user");
-
-// });
 
 module.exports = router;
