e88e9cc7f00f4f4d0912fee580b7a948531186d9
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2021-02-14 01:33:49 -0800

    cleaned up code

--- paper-homes/src/components/UserProfile.js
+++ paper-homes/src/components/UserProfile.js
@@ -40,47 +40,6 @@ class UserProfile extends Component {
           <br/>
           <div className="section-head">Mail Forwarding Address:</div>
           <div className="section-info">{ this.state.user["mailAddress"]}</div>
-          <div>{time}</div>
-          {/*<div>{ this.state.curtime} </div>*/}
-          {/* 
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card card-user">
-                <div className="card-image">
-                  <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..." />
-                </div>
-                <div className="card-body">
-                  <div className="author">
-                    <Link to='/'>
-                      <img className="avatar border-gray" src={face3} alt="..." />
-                      <h5 className="title">Mike Andrew</h5>
-                    </Link>
-                    <p className="description">
-                      michael24
-                    </p>
-                  </div>
-                  <p className="description text-center">
-                    "Lamborghini Mercy
-                      <br /> Your chick she so thirsty
-                      <br /> I'm in that two seat Lambo"
-                  </p>
-                </div>
-                <hr />
-                <div className="button-container mr-auto ml-auto">
-                  <button className="btn btn-simple btn-link btn-icon">
-                    <i className="fa fa-facebook-square"></i>
-                  </button>
-                  <button className="btn btn-simple btn-link btn-icon">
-                    <i className="fa fa-twitter"></i>
-                  </button>
-                  <button className="btn btn-simple btn-link btn-icon">
-                    <i className="fa fa-google-plus-square"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        */}
         </div>
       </div>
     )
